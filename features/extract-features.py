38d9d8c1466b35ecb24734e15d19eea1ca01419f
S Pradeep Kumar <gohanpra@gmail.com>
2017-10-31 21:30:26 -0400

    features: parse 3-over yaml file.

--- features/extract-features.py
+++ features/extract-features.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import scipy.io as sio
+import yaml
+import pprint
 
 def test_matrix_save():
     """Test how to save a matrix in the MATLAB format.
@@ -11,6 +13,23 @@ def test_matrix_save():
     print(a)
     sio.savemat('dummy-matrix.mat', {'a': a})
 
+def parse_yaml(yaml_file):
+    """
+    """
+    with open(yaml_file, 'r') as stream:
+        try:
+            data = yaml.load(stream)
+            pprint.pprint(data)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+def test_parse_yaml():
+    """Test parsing a YAML file.
+    """
+    f = "./raw-data/test-data/3-overs-335982.yaml"
+    parse_yaml(f)
+
 if __name__ == '__main__':
     print('Winning Team: ML on IPL\n')
-    test_matrix_save()
+    # test_matrix_save()
+    test_parse_yaml()
