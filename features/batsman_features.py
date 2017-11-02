bdebe50818d62fa7c453bc73c824ab45b33758b3
S Pradeep Kumar <gohanpra@gmail.com>
2017-11-01 23:15:50 -0400

    match: flatten yaml dictionary.

--- features/batsman_features.py
+++ features/batsman_features.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
 
+def flatten_list_of_dicts(dict_list):
+    flat_dict = {}
+    for d in dict_list:
+        assert len(d.keys()) == 1
+        for k,v in d.items():
+            assert k not in flat_dict
+            flat_dict[k] = v
+    return flat_dict
+
+class Match:
+    def __init__(self, yaml_data):
+        self.yaml_data = yaml_data
+
+    def flatten_cricket_yaml_data(self, yaml_data):
+        yaml_data['innings'] = flatten_list_of_dicts(yaml_data['innings'])
+        yaml_data['innings']['1st innings']['deliveries'] = flatten_list_of_dicts(yaml_data['innings']['1st innings']['deliveries'])
+        yaml_data['innings']['2nd innings']['deliveries'] = flatten_list_of_dicts(yaml_data['innings']['2nd innings']['deliveries'])
+        return yaml_data
+
 def get_ball(match_data, innings, ball_index, ball_number):
     """Get ball (for test purposes).
     """
--- features/batsman_features_test.py
+++ features/batsman_features_test.py
@@ -9,6 +9,45 @@ class TestBatsmenFeatures(unittest.TestCase):
     def setUp(self):
         yaml_file = "./raw-data/test-data/3-overs-335982.yaml"
         self.yaml_data = parse_yaml(yaml_file)
+        self.ball_1_point_2 = {
+            'batsman': 'BB '
+            'McCullum',
+            'bowler': 'Z Khan',
+            'non_striker': 'SC '
+            'Ganguly',
+            'runs': {
+                'batsman': 4,
+                'extras': 0,
+                'total': 4
+            }
+        }
+        self.ball_2_point_2 = {
+            'batsman': 'V Kohli',
+            'bowler': 'AB Dinda',
+            'non_striker': 'W '
+            'Jaffer',
+            'runs': {
+                'batsman': 0,
+                'extras': 0,
+                'total': 0
+            },
+            'wicket': {
+                'kind': 'bowled',
+                'player_out': 'V '
+                'Kohli'
+            }
+        }
+
+    def test_flatten_list_of_dicts(self):
+        self.assertEqual(flatten_list_of_dicts([{1: 2}, {2: 3}]),
+                         {1: 2, 2: 3})
+
+    def test_flatten_yaml_dict(self):
+        flat_innings = Match(None).flatten_cricket_yaml_data(self.yaml_data)['innings']
+        self.assertEqual(flat_innings['1st innings']['deliveries'][1.2],
+                         self.ball_1_point_2)
+        self.assertEqual(flat_innings['2nd innings']['deliveries'][2.2],
+                         self.ball_2_point_2)
 
     def test_runs_off_ball(self):
         self.assertEqual(120, 120)
