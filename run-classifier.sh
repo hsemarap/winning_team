c21b1a0bbf2421dfb01af6c7f25a817ba1b0e429
S Pradeep Kumar <gohanpra@gmail.com>
2017-11-04 17:24:56 -0400

    test: allow per-test execution; add flat yaml test file.

--- run-classifier.sh
+++ run-classifier.sh
@@ -2,4 +2,8 @@
 
 # ./features/extract_features.py
 
-python3 ./features/batsman_features_test.py
+PYTHONPATH=`pwd`:`pwd`/features:$PYTHONPATH
+export PYTHONPATH
+
+python3 -m unittest features.batsman_features_test
+# python3 -m unittest features.batsman_features_test.TestBatsmenFeatures.test_flatten_yaml_dict
