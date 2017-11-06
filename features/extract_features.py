662bc80526e5d58fcf46b447afdddc2bdd3f7c8b
S Pradeep Kumar <gohanpra@gmail.com>
2017-11-06 03:46:38 -0500

    batsmen: generate stats with default values.

--- features/extract_features.py
+++ features/extract_features.py
@@ -66,11 +66,11 @@ def test_season_2_with_season_1_stats():
     season2_dir = './raw-data/ipl/season-2-2009'
     num_files = None
     season1_matches = get_matches(season1_dir, num_files)
-    num_files = 1
-    # num_files = None
+    # num_files = 1
+    num_files = None
     season2_matches = get_matches(season2_dir, num_files)
-    last = 1
-    # last = len(season2_matches)
+    # last = 1
+    last = len(season2_matches)
     matrix = []
     outcome_vector = []
     for match in season2_matches[:last]:
--- run-classifier.sh
+++ run-classifier.sh
@@ -5,5 +5,5 @@
 PYTHONPATH=`pwd`:`pwd`/features:$PYTHONPATH
 export PYTHONPATH
 
-python3 -m unittest features.batsman_features_test
+# python3 -m unittest features.batsman_features_test
 # python3 -m unittest features.batsman_features_test.TestBatsmenFeatures.test_flatten_yaml_dict
