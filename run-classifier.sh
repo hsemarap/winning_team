db8a9fc1a89c62f4a8c12edd342c2bddc6f183fd
S Pradeep Kumar <gohanpra@gmail.com>
2017-12-02 19:58:52 -0500

    script: run tests.

--- run-classifier.sh
+++ run-classifier.sh
@@ -5,5 +5,5 @@
 PYTHONPATH=`pwd`:`pwd`/features:$PYTHONPATH
 export PYTHONPATH
 
-# python3 -m unittest features.batsman_features_test features.extract_features_test
-# python3 -m unittest features.batsman_features_test.TestBatsmenFeatures.test_flatten_yaml_dict
+python3 -m unittest features.batsman_features_test features.extract_features_test
+# python3 -m unittest features.batsman_features_test.TestBatsmenFeatures.test_team_bowling_average_plus_strike_rate_plus_economy
